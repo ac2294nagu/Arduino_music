ef6132af3a4110f6c6d5b4323305acaae2a3342f
unknown <ac2294nagu@PC302.ed.y.kumamoto-nct.ac.jp>
2015-06-03 10:53:18 +0900

    Arduinoを用いた電子オルゴール

---
+++ music/music.ino
@@ -0,0 +1,132 @@
+const int loudspeaker_pin = 13;
+const int base_time = 16 * 100; // 16の倍数が都合がいい
+
+void setup()
+{
+  pinMode( loudspeaker_pin, OUTPUT);
+}
+
+void loop()
+{
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 2);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 2);
+
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 523);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 8);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 8);
+  tone(loudspeaker_pin, 349);
+  delay(base_time / 2);
+  tone(loudspeaker_pin, 330);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 262);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 330);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 349);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 330);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 330);
+  delay(base_time / 8);
+  tone(loudspeaker_pin, 262);
+  delay(base_time / 8);
+  tone(loudspeaker_pin, 247);
+  delay(base_time / 2);
+
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 523);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 8);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 8);
+  tone(loudspeaker_pin, 349);
+  delay(base_time / 2);
+  tone(loudspeaker_pin, 330);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 262);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 330);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 349);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 330);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 330);
+  delay(base_time / 8);
+  tone(loudspeaker_pin, 262);
+  delay(base_time / 8);
+  tone(loudspeaker_pin, 247);
+  delay(base_time / 2);
+
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 2);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 2);
+  tone(loudspeaker_pin, 330);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 349);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 494);
+  delay(base_time / 8);
+  tone(loudspeaker_pin, 440);
+  delay(base_time / 8);
+  tone(loudspeaker_pin, 349);
+  delay(base_time / 4);
+  tone(loudspeaker_pin, 330);
+  delay(base_time / 1);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
